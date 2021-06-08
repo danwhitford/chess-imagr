c7532958b3cd2b986363f568b000693750917ed4
danielwhitford <whitford.dsw@gmail.com>
2021-06-08 21:08:02 +0100

    just output to terminal

--- cli.go
+++ cli.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"path/filepath"
 	"os"
+	"github.com/notnil/chess"
 )
 
 func main() {
@@ -15,6 +16,18 @@ func main() {
 	matching, _ := filepath.Glob(*glob)
 	for _, v := range matching {
 		fmt.Println(v)
+		pgnReader, err := os.Open(v)
+		if err != nil {
+			panic(err)
+		}
+		pgn, err := chess.PGN(pgnReader)
+		if err != nil {
+			panic(err)
+		}
+		game := chess.NewGame(pgn)
+		for _, pos := range game.Positions() {
+			fmt.Println(pos.Board().Draw())
+		}
 	}
 
 	os.Mkdir(*output, 0755)
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shatoe44/chess-imagr
 
 go 1.15
+
+require github.com/notnil/chess v1.5.0 // indirect
